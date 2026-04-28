14451e6abf89db8d25961d916a13d7ae49e1fc8f
007irr07-pixel <007irr07-pixel@users.noreply.github.com>
2026-04-28 22:39:30 +0500

    fix: remove distDir causing Vercel build failure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "../../../../../gtime-next",
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'i.ibb.co.com' },
